aec7ba67a0de5faa3d362403f4f4ebc3dcba036e
Raymond Li <rlii0303@gmail.com>
2021-07-08 13:51:58 -0700

    updated just fetching user_id

--- app/controllers/favorite_drinks_controller.rb
+++ app/controllers/favorite_drinks_controller.rb
@@ -1,5 +1,6 @@
 class FavoriteDrinksController < ApplicationController
     def index 
+      p params[:user_id]
       favorite_drinks = FavoriteDrink.all
       render json: favorite_drinks, include: :drink
     end    
