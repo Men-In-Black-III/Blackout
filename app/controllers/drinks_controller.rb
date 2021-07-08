f3f8c147512f0702ead5f91640b6f190c27308c9
Raymond Li <rlii0303@gmail.com>
2021-07-08 16:35:34 -0700

    changed controller to drinks

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,3 +1,17 @@
 class DrinksController < ApplicationController
-    
+    def index
+        if !params[:user_id]
+            render json: "There is no user", status:422
+          else 
+            user = User.find(params[:user_id]);
+            render json: user.drinks
+          end 
+    end
+
+    def create
+        Drinks.create(name:params[:name])
+    end
+
+
+
 end
--- app/controllers/favorite_drinks_controller.rb
+++ app/controllers/favorite_drinks_controller.rb
@@ -1,7 +1,5 @@
 class FavoriteDrinksController < ApplicationController
-    def index 
-      p params[:user_id]
-      favorite_drinks = FavoriteDrink.all
-      render json: favorite_drinks, include: :drink
-    end    
+    
 end
+
+
