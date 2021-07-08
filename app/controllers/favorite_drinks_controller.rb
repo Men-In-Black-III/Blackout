371c9acd19d3f83be7e257be7f4af1bf5ca2fecf
Raymond Li <rlii0303@gmail.com>
2021-07-08 11:33:17 -0700

    added seed data and getting drinks from drink table

--- app/controllers/favorite_drinks_controller.rb
+++ app/controllers/favorite_drinks_controller.rb
@@ -1,11 +1,6 @@
 class FavoriteDrinksController < ApplicationController
     def index 
-       @favorite_drinks = FavoriteDrink.all
-       render json: @favorite_drinks 
-    end
-
-    def show
-       @favorite_drink = FavoriteDrink.find(params[:id]) 
-       render json: @favorite_drink
-    end
+      favorite_drinks = FavoriteDrink.all
+      render json: favorite_drinks, include: :drink
+    end    
 end
